1ca7a12dcd7a4d759dcfcd67af33848ec8038f4a
Francois <francois-lolin@live.fr>
2023-05-12 17:54:50 +0200

    save_commit

--- srcs/requirements/mariadb/conf/mariadb.sh
+++ srcs/requirements/mariadb/conf/mariadb.sh
@@ -1,8 +1,18 @@
-service mysql start;
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
-exec mysqld_safe
+if [ ! -f "/var/lib/mysql/file_flag_mdb_done" ]; then
+	service mysql start
+	sleep 3
+	while [ ! -e /var/run/mysqld/mysqld.sock ]; do
+		sleep 1
+	done
+	mysql << EOF
+CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;
+CREATE USER IF NOT EXISTS \`${SQL_USER}\`@\`localhost\` IDENTIFIED BY '${SQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';
+FLUSH PRIVILEGES;
+EOF
+	sleep 5
+	mysqladmin -uroot -p$SQL_ROOT_PASSWORD shutdown
+	touch /var/lib/mysql/file_flag_mdb_done
+fi
+exec mysqld
--- srcs/requirements/wordpress/conf/wp_conf.sh
+++ srcs/requirements/wordpress/conf/wp_conf.sh
@@ -1,10 +1,30 @@
 if [ ! -f /var/www/wordpress/wp-config.php ]; then
 	sleep 10
-	wp config create	--allow-root \
-						--dbname=$SQL_DATABASE \
-						--dbuser=$SQL_USER \
-						--dbpass=$SQL_PASSWORD \
-						--dbhost=mariadb:3306 --path='/var/www/wordpress'
+	# wp core download --path='/var/www/wordpress'
+	wp config create --allow-root \
+		--dbname=$SQL_DATABASE \
+		--dbuser=$SQL_USER \
+		--dbpass=$SQL_PASSWORD \
+		--dbhost=mariadb:3306 \
+		--path='/var/www/wordpress'
+	sleep 10
+	# configurer la page automatiquement
+	wp core install --allow-root \
+		--url=$WP_SITE \
+		--title=$WP_TITLE \
+		--admin_user=$WP_ADMIN \
+		--admin_password=$WP_ADMIN_PASSWORD \
+		--admin_email=$WP_ADMIN_EMAIL \
+		--path='/var/www/wordpress'
+	# creer un second utilisateur
+	wp user create --allow-root \
+		"$WP_USER" \
+		"$WP_USER_EMAIL" \
+		--role=author \
+		--user_pass=$WP_USER_PASSWORD \
+		--path='/var/www/wordpress'
+else
+	echo "wp-config php DONE"
 fi
 
 if [ ! -d /run/php ]; then
